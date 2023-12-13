3bc2a5c59910280b9e7bc7ff54fdafc519bb8f1c
shumman <shumman2007@hotmail.com>
2023-12-13 06:05:12 +0500

    added server files

--- app.py
+++ app.py
@@ -6,19 +6,30 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "https://app.kolortrak.com"}})
+CORS(app)
 
-@app.route("/",methods=['POST'])
-def index():
-    fund_codes_to_search = request.get_json()
+# @app.route("/",methods=['POST'])
+# def index():
+#     fund_codes_to_search = request.get_json()
+#     # fund_codes_to_search =["BMO97334", "HMUS.U", "BMO97325", "PFC6611"]
 
-    suggested_funds_result = get_suggested_funds(
-        "static/Funds.xlsx", fund_codes_to_search
-    )
-    return jsonify(suggested_funds_result)
+#     suggested_funds_result = get_suggested_funds(
+#         "api/static/Funds.xlsx", fund_codes_to_search
+#     )
+#     return jsonify(suggested_funds_result)
     # return render_template("index.html", suggested_funds=suggested_funds_result)
 
+@app.route("/")
+def index():
+    # fund_codes_to_search = request.get_json()
+    # fund_codes_to_search =["BMO97334", "HMUS.U", "BMO97325", "PFC6611"]
+
+    # suggested_funds_result = get_suggested_funds(
+    #     "static/Funds.xlsx", fund_codes_to_search
+    # )
+    # return jsonify(suggested_funds_result)
+    return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
